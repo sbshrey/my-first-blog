8fb1b345a936bbe6a15382861c8511dee3a0e624
Shrey Baheti <sbshrey@gmail.com>
2016-07-04 13:42:03 +0530

    settingsviews and base html file changed

--- blog/views.py
+++ blog/views.py
@@ -16,56 +16,69 @@ logger = logging.getLogger(__name__)
 # Create your views here.
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'blog/post_list.html', context)
 
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+    context = {
+        'post': post,
+    }
+    return render(request, 'blog/post_detail.html', context)
 
 
 @login_required
 def post_new(request):
-    if request.method == "POST":
-        form = PostForm(request.POST, request.FILES or None)
+    form = PostForm(request.POST or None, request.FILES or None)
 
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            # post.published_date = timezone.now()
-            post.save()
-            messages.success(request, "Successfully Created")
-            return redirect('post_detail', pk=post.pk)
-        else:
-            messages.error(request, "Not Successfully Created")
+    if form.is_valid():
+        post = form.save(commit=False)
+        post.author = request.user
+        # post.published_date = timezone.now()
+        post.save()
+        messages.success(request, "Successfully Created")
+        return redirect('post_detail', pk=post.pk)
     else:
-        form = PostForm()
+        messages.error(request, "Not Successfully Created")
 
-    return render(request, 'blog/post_form.html', {'form': form})
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'blog/post_form.html', context)
 
 
 @login_required
 def post_edit(request, pk):
-    instance = get_object_or_404(Post, pk=pk)
-    if request.method == "POST":
-        form = PostForm(request.POST, request.FILES or None, instance=instance)
-        if form.is_valid():
-            instance = form.save(commit=False)
-            instance.author = request.user
-            # post.published_date = timezone.now()
-            instance.save()
-            messages.success(request, "Post Saved")
-            return redirect('post_detail', pk=instance.pk)
-    else:
-        form = PostForm(request.POST, instance=instance)
+    post = get_object_or_404(Post, pk=pk)
+    form = PostForm(request.POST or None, request.FILES or None, instance=post)
+    if form.is_valid():
+        post = form.save(commit=False)
+        post.author = request.user
+        # post.published_date = timezone.now()
+        post.save()
+        messages.success(request, "Post Saved")
+        return redirect('post_detail', pk=post.pk)
 
-    return render(request, 'blog/post_form.html', {'form': form})
+    context = {
+        'title': post.title,
+        'instance': post,
+        'form': form,
+    }
+
+    return render(request, 'blog/post_form.html', context)
 
 
 @login_required
 def post_draft_list(request):
     posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
-    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'blog/post_draft_list.html', context)
 
 
 @login_required
@@ -74,7 +87,7 @@ def post_publish(request, pk):
     if request.user.is_superuser and post.published_date is None:
         post.publish()
         messages.success(request, "Post published")
-    return redirect('blog.views.post_list')
+    return redirect(post_list)
 
 
 @login_required
@@ -89,25 +102,30 @@ def post_remove(request, pk):
 @login_required
 def add_comment_to_post(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    if request.method == "POST":
-        form = CommentForm(request.POST)
-        if form.is_valid():
-            comment = form.save(commit=False)
-            comment.author = request.user
-            comment.post = post
-            comment.save()
-            messages.success(request, "Comment is added")
-            messages.success(request, "Thank you for the comment")
-            return redirect('blog.views.post_detail', pk=post.pk)
-    else:
-        form = CommentForm()
-    return render(request, 'blog/add_comment_to_post.html', {'form': form})
+    form = CommentForm(request.POST or None)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.author = request.user
+        comment.post = post
+        comment.save()
+        messages.success(request, "Comment is added")
+        messages.success(request, "Thank you for the comment")
+        return redirect(post_detail, pk=post.pk)
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'blog/add_comment_to_post.html', context)
 
 
 def post_comment_draft_list(request):
     posts = Post.objects.order_by('-created_date')
     # comments = Comment.objects.filter(published_date__isnull=True).order_by('created_date')
-    return render(request, 'blog/post_comment_draft_list.html', {'posts': posts})
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'blog/post_comment_draft_list.html', context)
 
 
 @login_required
@@ -116,7 +134,7 @@ def comment_approve(request, pk):
     if request.user.is_superuser or comment.post.author == request.user:
         comment.approve()
         messages.success(request, "Comment approved")
-    return redirect('blog.views.post_detail', pk=comment.post.pk)
+    return redirect(post_detail, pk=comment.post.pk)
 
 
 @login_required
@@ -126,4 +144,4 @@ def comment_remove(request, pk):
     if comment.author == request.user or request.user.is_superuser or comment.post.author == request.user:
         comment.delete()
         messages.success(request, "Comment removed")
-    return redirect('blog.views.post_detail', pk=post_pk)
+    return redirect(post_detail(), pk=post_pk)
